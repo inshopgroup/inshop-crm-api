55042d2ff0da13ce0a1874e96a5f532586fe1d51
Maksym Mukovoz <info@inshop.com.ua>
2019-09-03 15:19:04 +0200

    Prevent to remove record which has relations

---
+++ src/EventListener/EntityRemoveListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Exception\EntityRemoveException;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+/**
+ * Class EntityRemoveListener
+ * @package App\EventListener
+ */
+class EntityRemoveListener
+{
+    /**
+     * @param LifecycleEventArgs $args
+     * @throws EntityRemoveException
+     */
+    public function preRemove(LifecycleEventArgs $args)
+    {
+        $em = $args->getEntityManager();
+        $entity = $args->getObject();
+
+        $associationNames = $em->getClassMetadata(get_class($entity))->getAssociationNames();
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        foreach ($associationNames as $name) {
+            $association = $accessor->getValue($entity, $name);
+
+            if ($association instanceof Collection && $association->count()) {
+                throw new EntityRemoveException('Can not remove entity');
+            }
+        }
+    }
+}
---
+++ src/Exception/EntityRemoveException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exception;
+
+/**
+ * Class EntityRemoveException
+ * @package App\Exception
+ */
+class EntityRemoveException extends \Exception
+{
+}
