438200df692b57a80be3fce7c353410d67b3305d
Mykyta Shtefanesa <true.fane@gmail.com>
2021-01-20 14:35:25 +0100

    prepare for test

--- config/preload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
-    opcache_compile_file(dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php');
-}
