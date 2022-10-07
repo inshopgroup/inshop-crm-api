3ccebacd3bac4c80c6c45b4e3a9d5070f39efdb6
Maksym Mukovoz <info@inshop.com.ua>
2022-10-07 22:41:23 +0200

    Switched api platform annotations to php attributes

--- src/Entity/History.php
+++ src/Entity/History.php
@@ -3,71 +3,67 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
-use App\Repository\GroupRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use App\Controller\History\HistoryGetEntityCollectionAction;
 use App\Repository\HistoryRepository;
 use DateTime;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Core\Annotation\ApiResource;
-use App\Controller\History\HistoryGetEntityCollectionAction;
 use Symfony\Component\Serializer\Annotation\Groups;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 
-/**
- * @ApiResource(
- *     attributes={
- *          "normalization_context"={
- *              "groups"={
- *                  "history_read",
- *              }
- *          },
- *          "order"={
- *              "id": "DESC"
- *          }
- *     },
- *     collectionOperations={
- *          "get"={
- *              "security"="is_granted('ROLE_HISTORY_LIST')"
- *          },
- *          "getEntity"={
- *              "security"="is_granted('ROLE_HISTORY_LIST')",
- *              "method"="GET",
- *              "path"="/histories/{entity}/{entityId}",
- *              "normalization_context"={
- *                  "groups"={"history_get_entity_collection"}
- *              },
- *              "controller"=HistoryGetEntityCollectionAction::class,
- *              "defaults"={"_api_receive"=false},
- *          },
- *     },
- *     itemOperations={
- *          "get"={
- *              "security"="is_granted('ROLE_HISTORY_SHOW')"
- *          }
- *     },
- * )
- * @ApiFilter(DateFilter::class, properties={"loggedAt"})
- * @ApiFilter(SearchFilter::class, properties={
- *     "id": "exact",
- *     "action": "ipartial",
- *     "objectId": "ipartial",
- *     "objectClass": "ipartial",
- *     "username": "ipartial",
- * })
- * @ApiFilter(
- *     OrderFilter::class,
- *     properties={
- *          "id",
- *          "action",
- *          "objectId",
- *          "objectClass",
- *          "username",
- *          "loggedAt"
- *     }
- * )
- */
+#[ApiResource(
+    collectionOperations: [
+        'get' => [
+            'security' => "is_granted('ROLE_HISTORY_LIST')"
+        ],
+        'getEntity' => [
+            'security' => "is_granted('ROLE_HISTORY_LIST')",
+            'method' => 'GET',
+            'path' => '/histories/{entity}/{entityId}',
+            'normalization_context' => ['groups' => ["history_get_entity_collection"]],
+            'controller' => HistoryGetEntityCollectionAction::class,
+            'defaults' => ['_api_receive' => false]
+        ],
+    ],
+    itemOperations: [
+        'get' => ['security' => "is_granted('ROLE_HISTORY_SHOW')"],
+    ],
+    attributes: [
+        'order' => ['id' => "DESC"],
+        'normalization_context' => ['groups' => ["history_read"]],
+    ]
+)]
+#[ApiFilter(
+    DateFilter::class,
+    properties: [
+        "createdAt",
+        "updatedAt",
+    ]
+)]
+#[ApiFilter(
+    SearchFilter::class,
+    properties: [
+        "id" => "exact",
+        "action" => "ipartial",
+        "objectId" => "ipartial",
+        "objectClass" => "ipartial",
+        "username" => "ipartial",
+    ]
+)]
+#[ApiFilter(
+    OrderFilter::class,
+    properties: [
+        "id",
+        "action",
+        "objectId",
+        "objectClass",
+        "username",
+        "loggedAt"
+    ]
+)]
 #[ORM\Entity(repositoryClass: HistoryRepository::class)]
 class History
 {
